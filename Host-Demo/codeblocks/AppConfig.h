ae651164c2441f9aeb3ba607868b53ed1c89e8c2
Zapfenkiller <80218209+Zapfenkiller@users.noreply.github.com>
2021-12-07 19:59:20 +0100

    performs some white space cosmetics.

--- Host-Demo/codeblocks/AppConfig.h
+++ Host-Demo/codeblocks/AppConfig.h
@@ -10,11 +10,11 @@
  **************************************************************/
 
 
-#define  usbVID   0x04D8   // Microchip Inc.
-#define  usbPID   0xEBC7   // USB-Userport
-#define  usbSNR   "01"     // Device serial number
+#define  usbVID                  0x04D8   // Microchip Inc.
+#define  usbPID                  0xEBC7   // USB-Userport
+#define  usbSNR                  "01"     // Device serial number
 
-#define  REPORT_ID_DEVICE_LEDS   0x01  // Report ID of onboard LED control
+#define  REPORT_ID_DEVICE_LEDS   0x01     // Report ID of on-board LED control
 
-#define  RX_LED_BITMASK          0x01  // Bitmask to access yellow LED
-#define  TX_LED_BITMASK          0x02  // Bitmask to access green LED
+#define  RX_LED_BITMASK          0x01     // Bitmask to access yellow LED
+#define  TX_LED_BITMASK          0x02     // Bitmask to access green LED
